f214d3e2634329c4fb84e702a18242c44e79ab99
Florian.Popa <florian.popa@wolfpack-digital.com>
2018-09-10 16:14:14 +0300

    Implement advanced login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest 
-
-  test "valid signup information" do
+  test 'valid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
