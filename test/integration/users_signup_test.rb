e536f429f32c32ed5433ed8cd8938c4e0f97e6a3
Florian.Popa <florian.popa@wolfpack-digital.com>
2018-09-12 12:03:22 +0300

    Finish user edit,update,index and destroy actions

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
-class UsersSignupTest < ActionDispatch::IntegrationTest 
-  test 'valid signup information' do
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
@@ -10,11 +11,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.FILL_IN
     assert is_logged_in?
   end
 end
